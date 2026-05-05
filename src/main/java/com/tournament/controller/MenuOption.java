2fa1f632c23edb87c78d2c0d973177c165707ed1
SZMNNWK <sszymon.nnowak@gmail.com>
2026-05-05 14:16:28 +0200

    Mały controller pełno bugów :)

---
+++ src/main/java/com/tournament/controller/MenuOption.java
@@ -0,0 +1,23 @@
+package com.tournament.controller;
+
+public enum MenuOption {
+    CREATE("Create tournament"),
+    LIST("List tournaments"),
+    MANAGE("Manage tournament"),
+    DELETE("Delete tournament"),
+    EXIT("Exit");
+
+    private final String label;
+
+    MenuOption(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public static MenuOption fromInt(int i) {
+        return values()[i];
+    }
+}
